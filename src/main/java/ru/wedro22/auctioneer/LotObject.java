a61fc64612335905fde61dd3bf082e598916f83a
wedro22 <wedro2208@gmail.com>
2017-01-25 17:57:43 +0300

    LotObject

--- src/main/java/ru/wedro22/auctioneer/LotObject.java
+++ src/main/java/ru/wedro22/auctioneer/LotObject.java
@@ -1,35 +1,115 @@
 package ru.wedro22.auctioneer;
 
 
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import ru.wedro22.auctioneer.util.AChat;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by wedro on 22.01.2017.
  */
 public class LotObject{
-    private String name;
-    private Object lot;
-    private String nameT;
+    private ItemStack lot;
+
+    private EntityPlayerMP seller;      //продавец товара
+    private Date dateStart;     //дата старта аукциона
+    private Date dateEnd;       //дата конца аукциона
+    private Date dateServSaved; //дата сохранения сервера
+    private PricesList pricesList;//список вариантов продаж
+
+    public static final int PRICECOUNT = 5;    //лимит итемов в одной ставке
+    public static final int PRICESCOUNT = 5;   //лимит различных ставок для лота
+
+
+    /**
+     * Список вариантов продаж.
+     * (ограничение количества потому что так надо вот)
+     */
+    public class PricesList{
+        public PriceList[] pricesList = new PriceList[PRICESCOUNT];
+
+        /**
+         * @param prices - варианты продаж(с лимитом)
+         * @return заполненный список вариантов продаж
+         */
+        public PricesList addPricesList(PriceList... prices){
+            if (prices.length > PRICESCOUNT){
+                //ДОПИСАТЬ СООБЩЕНИЕ О ОШИБКЕ ПЕРЕПОЛНЕНИЯ
+                return null;
+            }
+            int i=0;
+            for (PriceList pl:prices) {//заполнение
+                this.pricesList[i]=pl;
+                i+=1;
+            }
+            return this;
+        }
+
+    }
+    public class PriceList{
+        public ItemStack[] itemsStack = new ItemStack[PRICECOUNT];
+
+        /**
+         * @param items - список итемов
+         * @return заполненный итемами вариант продажи
+         */
+        public PriceList addItems(ItemStack... items){
+            if (items.length > PRICECOUNT){
+                //ДОПИСАТЬ СООБЩЕНИЕ О ОШИБКЕ ПЕРЕПОЛНЕНИЯ
+                return null;
+            }
+            int i=0;
+            for (ItemStack is:items) {//заполнение
+                this.itemsStack[i]=is;
+                i+=1;
+            }
+            return this;
+        }
+    }
 
 
     /**
      * Use the verification before!
-     * @param lot - object for auction
+     * @param item - object for auction
      */
-    LotObject(Object lot){
-        this.name = addName(lot);
-        this.lot=lot;
-        this.nameT=lot.getClass().getName();
+    public void addLotFromGame(ItemStack item               ){//доделать
+        this.lot=item;
     }
 
-    public static boolean verification(Object lot) {
-        return true;                                    //TEST!!!
+    public LotObject addLotFromBD(){
+        return null;            //not yet ready!!!
     }
 
-    private static String addName(Object lot) {
-        return getIdString(lot);                        //VARIANT
+    public static boolean verification(ItemStack lot) {
+        return true;            //not yet ready!!!
     }
-    public static String getIdString(Object lot) {
-        return "testId"+String.valueOf(Math.random());  //TEST!!!
+
+    public String getName() {
+        return lot.getDisplayName();
     }
+    public int getId(){
+        return lot.getItemDamage();
+    }
+    public int getMeta(){
+        return lot.getMetadata();
+    }
+    public int getAmount(){
+        return lot.stackSize;
+    }
+    public NBTTagCompound getNbt(){
+        return lot.getTagCompound();
+    }
+
+
+
+
+}
+
+    /*UNUSED
     public Class getTClass(){
         Class c = null;
         try {
@@ -40,24 +120,19 @@ public class LotObject{
         return c;
     }
     public Object getTInstance(){
-        Class c = null;
-        try {
-            c = Class.forName(this.nameT);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            return c.newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        Class c = getTClass();
+        if (c!=null) {
+            try {
+                return c.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
-
-}
-
+    */
 
 //package net.minecraftforge.fml.common.registry.IForgeRegistryEntry.Impl;
 //package net.minecraft.client.gui.MapItemRenderer;
